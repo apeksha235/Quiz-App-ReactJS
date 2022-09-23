c25d26787f43f4083a189bb9fe218bc019a68ef1
Apeksha Hegde <84583787+apeksha235@users.noreply.github.com>
2022-09-23 16:33:15 +0530

    Update Footer.js

--- quiz-app/src/components/Footer/Footer.js
+++ quiz-app/src/components/Footer/Footer.js
@@ -2,6 +2,10 @@ import React from 'react'
 import './Footer.css'
 
 const Footer = () => {
+   window.dataLayer = window.dataLayer||[];
+  function gtag()(dataLayer.push(arguments);}
+ gtag(js',new Date());
+ gtag('config','UA-242613192-1)
   return (
     <footer className='footer'>Made with ❤ by Apeksha Hegde
        <div><a href="https://github.com/apeksha235">Github</a></div>
